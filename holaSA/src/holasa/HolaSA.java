a187c4fa1816d141f257f836810612e6a2783515
Cesar Luis Rodriguez Solera <cesarco@localhost.localdomain>
2022-05-21 22:28:06 -0500

    inputs por consola

--- holaSA/src/holasa/HolaSA.java
+++ holaSA/src/holasa/HolaSA.java
@@ -4,13 +4,17 @@
  */
 package holasa;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author cesarco
  */
 public class HolaSA {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //                // creando una variable de tipo string
 //      String nombre="Cesar Luis";
 //      
@@ -23,16 +27,22 @@ public class HolaSA {
 //      
 //      int edad = 19;
 //      System.out.println(edad);
+//
+//        int edad = 5;
+//        boolean esMayorDeEdad = edad >= 18;
+//
+//        if (esMayorDeEdad) {
+//            System.out.println("Es mayor de edad");
+//        } else {
+//            System.out.println("Es menor de edad");
+//        }
 
-        int edad = 5;
-        boolean esMayorDeEdad = edad >= 18;
 
-        if (esMayorDeEdad) {
-            System.out.println("Es mayor de edad");
-        } else {
-            System.out.println("Es menor de edad");
-        }
-
+InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+BufferedReader reader = new BufferedReader(inputStreamReader);
+System.out.println("Ingrese su nombre:");
+String name = reader.readLine();
+System.out.println("Hola " + name);
     }
 
 }
