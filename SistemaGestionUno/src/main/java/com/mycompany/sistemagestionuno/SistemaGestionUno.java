755efb03bd6e5afb4a67c9a450bcb4e4e257ecab
Cesar Luis Rodriguez Solera <cesarco@localhost.localdomain>
2022-06-01 09:08:17 -0500

    proyecto maven bd

---
+++ SistemaGestionUno/src/main/java/com/mycompany/sistemagestionuno/SistemaGestionUno.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sistemagestionuno;
+
+import com.mycompany.sistemagestionuno.forms.formularioClientes;
+
+/**
+ *
+ * @author cesarco
+ */
+public class SistemaGestionUno {
+
+    public static void main(String[] args) {
+       formularioClientes ventana = new formularioClientes();
+                ventana.show();
+    }
+}
