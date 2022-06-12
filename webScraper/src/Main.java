744276a00efa65ced2ee9ad8c96f5d559d56e799
cesarco <devcesarco@outlook.es>
2022-06-12 16:06:42 -0500

    Webscraper con Concurrencia

--- webScraper/src/Main.java
+++ webScraper/src/Main.java
@@ -5,21 +5,64 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         // Donwload webs
 
-        String link = "https://platzi.com/";
-        URL url = new URL(link);
-        HttpURLConnection coon = (HttpURLConnection) url.openConnection();
-        String encoding = coon.getContentEncoding();
+            List<String> links = new ArrayList<>();
+            links.add("https://platzi.com/");
+            links.add("https://platzi.com/clases/1050-programacion-basica/5103-mi-primera-linea-de-codigo/");
+            links.add("https://platzi.com/categorias/negocios/");
+            links.add("https://platzi.com/categorias/desarrollo/");
 
-        InputStream input = coon.getInputStream();
-        String result = new BufferedReader(new InputStreamReader(input)).
-                lines().collect(Collectors.joining());
-        System.out.println(result);
+        links.stream().parallel().forEach(link -> getWebContent(link));
+
+
+            String link = "https://platzi.com/";
+            String result = getWebContent(link);
+
+
 
     }
+    private synchronized static String getWebContent(String link){
+        System.out.println("INIT");
+        System.out.println(link);
+
+        try{
+            URL url = new URL(link);
+            HttpURLConnection coon = (HttpURLConnection) url.openConnection();
+            String encoding = coon.getContentEncoding();
+
+            InputStream input = coon.getInputStream();
+            Stream<String> lines = new BufferedReader(new InputStreamReader(input)).lines();
+/*
+            List<String> array = new ArrayList<>();
+            array.add("<html>");
+            array.add("<head>");
+            array.add("<title>");
+            array.forEach(line -> System.out.println(line));
+*/
+            System.out.println("END");
+
+            saveInDatabase(result);
+            return lines.collect(Collectors.joining());
+
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+        return "";
+    }
+
+    public static void saveInDatabase(String x){
+
+    }
+
 }
