b162794ea4c5eaab887c6f6a12e21fd9ae738602
cesarco <devcesarco@outlook.es>
2022-06-13 16:34:34 -0500

    variables atomicas

--- varAtomicas/src/Main.java
+++ varAtomicas/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
 
     public static void main(String[] args) throws  Exception{
@@ -7,22 +9,33 @@ public class Main {
         System.out.println("end");
 
  */
+
         Counter counter = new Counter();
         Thread first = new Thread(counter, "First");
         Thread second = new Thread(counter, "second");
 
         first.start();
         second.start();
-        Thread.sleep(5000);
+
+        first.join();
+        second.join();
+
+       // Thread.sleep(5000);
         System.out.println(counter.count);
     }
     static class Counter extends Thread {
-        public int count = 0;
+        public AtomicInteger count =  new AtomicInteger(0);
 
         public void run(){
             for (int i = 0; i<200_000_000; i++){
-                count++;
+                count.addAndGet(1);
             }
         }
     }
 }
+
+/*
+*  Las variables atomicas son utilizadas para porder ejecutar dos procesos que esta utilizando un mismo recurso
+* es decir, en este caso  que la variable count solo va a ejecutar un proceso a la vez evitar incovenientes a la
+* hora de msotrar en consola. Ahi diferentes tipos variables atomicas con long, boolean entre otros.
+* */
