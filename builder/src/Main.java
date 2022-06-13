7d8ed43d0f80013919fc06fae98d636ee8ba63c4
cesarco <devcesarco@outlook.es>
2022-06-13 18:23:13 -0500

    NUllPointerException

---
+++ builder/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Person Cesar = new Person();
+        Cesar.setName("Cesar Luis");
+        Cesar.setEmail("devcesarco@gmail.com");
+
+       if( Cesar.getLastName().equals("Rodriguez")){
+
+       }
+
+    }
+}
