146357aeb39fec1889e345f379f5b049c8e9e43a
cesarco <devcesarco@outlook.es>
2022-06-30 15:08:01 -0500

    optional con arrays y lambda

--- optional/src/Main.java
+++ optional/src/Main.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args){
+
+        List<String> countries = new ArrayList<>();
+        countries.add("United States");
+        countries.add("Argentina");
+        countries.add("Spain");
+        countries.add("Italy");
+        countries.add("Colombia");
+        countries.add("Mexico");
+        countries.add("Ecuador");
+        countries.add("Peru");
+
+    Optional<String> country = countries.stream()
+            .filter(c -> c.startsWith("Arg")).findFirst();
+    //findFirst -> devuelve el primer resultado, pero envuelto en un optinal
+
+       // if(country.isPresent()) System.out.println(country.get());
+        country.ifPresent(System.out::println);
+
+    }
+
+    public static void exampleOptional(){
         Optional<Double> result = averageScores(7.0, 8.0, 4.0,10.0);
         if(result.isPresent()){
             System.out.println(result.get()); // de esta forma nos permite obtener el valor que esta dentro de optional
@@ -10,6 +33,7 @@ public class Main {
             System.out.println("Nothing");
         }
     }
+
     public static Optional<Double> averageScores(Double ...scores){
         if(scores.length == 0){
             return Optional.empty();
