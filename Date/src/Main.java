a4b6fb871abc9d0928011bfadc052ac86652cb27
cesarco <devcesarco@outlook.es>
2022-07-01 19:42:02 -0500

    Calendar vs LocalDateTime

--- Date/src/Main.java
+++ Date/src/Main.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 public class Main {
@@ -17,9 +18,16 @@ public class Main {
         LocalTime time = LocalTime.of(10,0);
 
         // DateTime
-        LocalDateTime dt = LocalDateTime.of(date,time);
+        LocalDateTime dateTime = LocalDateTime.of(date,time);
 
-        LocalDateTime dt2 = LocalDateTime.of(2022, 1,1, 10,1);
+        LocalDateTime dateTime2= LocalDateTime.of(2022, 1,1, 10,1);
     // localdatetime maneja dos objetos, el objeto time y el objetos date por separador
+
+        LocalDateTime lastWeek = dateTime.minusDays(7); // resta dias
+        dateTime.plusDays(2); // sumar
+
+        String dateFormated = dateTime.format(DateTimeFormatter.ISO_DATE_TIME);
+        // es un formato en el podemos personalizar el tiempo o la fecha
+        System.out.println(dateFormated);
     }
 }
